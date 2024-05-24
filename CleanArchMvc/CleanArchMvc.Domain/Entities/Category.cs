90b7c9192d18a7ed6b41aed1cd0b562683dd7b90
Fernandanery <fernanda.a.nery@hotmail.com>
2024-05-24 11:22:27 -0300

    iniciando validações e ajustando classes no dominio

--- CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -1,9 +1,36 @@
-﻿namespace CleanArchMvc.Domain.Entities
+﻿using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Product> Products { get; set;}
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        // Construtores parametrizados para permitir criar os objetos
+        public Category(string name)
+        {
+            ValidateDomain(name);  
+        }
+
+        // Esse construtor será utilizado para popular os valores na tabela
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+        public ICollection<Product> Products { get; private set;}
+
+        // Lógica de validação do nome vazio ou quantidade de caracteres
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short minimal 3 characteres");
+
+            Name = name;
+
+        }
     }
 }
--- CleanArchMvc/CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -1,6 +1,20 @@
 ﻿namespace CleanArchMvc.Domain.Validation
 {
-    internal class DomainExceptionValidation
+    public class DomainExceptionValidation : Exception
     {
+        // Excessões para validação do dominio
+        public DomainExceptionValidation(string error) : base(error)
+        { 
+
+        }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExceptionValidation(error);
+            }
+            
+        }
     }
 }
