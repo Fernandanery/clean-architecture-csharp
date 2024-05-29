91e742deb538ad99bce22221c52b0073f1439d20
Fernandanery <fernanda.a.nery@hotmail.com>
2024-05-28 21:30:20 -0300

    WIP - iniciando implementação dos testes unitários na classe Product

--- CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ff126f804c0ad5f5b53479ac3453cb2c01d317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b98512c4125700722704fc79710c4ad37454fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
