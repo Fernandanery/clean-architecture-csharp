eea7165e553ee156d8b2b58aeb5acfc2c1c35fc6
Fernandanery <fernanda.a.nery@hotmail.com>
2024-05-29 21:17:54 -0300

    inciando a implementação da camada da infra

--- CleanArchMvc/CleanArchMvcInfra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvcInfra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf63914ef96e3d876e2d1927530e07730752bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ebe37add330b17cc08b7265702d3564dd7c9af")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
