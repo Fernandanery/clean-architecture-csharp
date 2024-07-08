92b04ebb9fddfb7b02fca8c01456afc011272f99
Fernandanery <fernanda.a.nery@hotmail.com>
2024-07-08 17:27:45 -0300

    Criando os DTOS e mapeamentos com autoMapper

---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        //Abaixo data annotations de Name
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+
+
+    }
+}
--- CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4b8e1a5286a4d99eaaf7233c705ebd087bffb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef4952eef03b82223dcb9b7691b24ad6ba7326e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
