efe1c3275a5d6f649dc8cbe8f640c5a98f7a4676
Fernandanery <fernanda.a.nery@hotmail.com>
2024-07-10 12:50:29 -0300

    wip - iniciando implementação do padrão CQRS

--- CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7287094da3c68c01133b00214f1eed1b01493953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01342b95a575e9d9e47ce63715b12742b8e44e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
