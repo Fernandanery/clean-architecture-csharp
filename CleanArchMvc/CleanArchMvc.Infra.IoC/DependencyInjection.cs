5f56f39dabae6bcfb435e827e443207d0ae26050
Fernandanery <fernanda.a.nery@hotmail.com>
2024-07-16 21:42:19 -0300

    feat- wip: implementando autorizações

--- CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,13 +23,28 @@ namespace CleanArchMvc.Infra.IoC
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
                 ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            // registrar o serviço do identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            // Configuração do cookie
+            services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Account/Login");
+
             // registar os repositories
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
+            // registar os services
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
             // registrar os services
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
+
+            // registrar os serviços do identity
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
             // registrar o auto mapper
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
