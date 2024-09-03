a53a84d6de4a728a6d3dbb9e46ff8f0fa1c9fd18
Fernandanery <fernanda.a.nery@hotmail.com>
2024-09-03 16:26:44 -0300

    feat: imlementando o productController

--- CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43e6690b09bfe1e546623f808d2797a51db1ae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d62b8abd6b59dbc4fae8a434e5d22f3476e8f78")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CleanArchMvc/CleanArchMvc.WebApi/Controllers/ProductsController.cs
+++ CleanArchMvc/CleanArchMvc.WebApi/Controllers/ProductsController.cs
@@ -1,11 +1,88 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMvc.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(IProductService productService) : ControllerBase
     {
+        private readonly IProductService _productService = productService;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> Products()
+        {
+            var products = await _productService.GetProducts();
+
+            if (products == null)
+            {
+                return NotFound("Products not found");
+            }
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}", Name = "ProductdById")]
+        public async Task<ActionResult<ProductDto>> ProductdById(int id)
+        {
+            var product = await _productService.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct([FromBody] ProductDto productDto)
+        {
+            if (productDto == null)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            await _productService.Add(productDto);
+
+            return new CreatedAtRouteResult("ProductdById", new { id = productDto.Id }, productDto);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, [FromBody] ProductDto productDto)
+        {
+
+            if (id != productDto.Id)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            if (productDto == null)
+            {
+                return BadRequest();
+            }
+
+            await _productService.Update(productDto);
+
+            return Ok(productDto);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<ProductDto>> RemoveProduct(int id)
+        {
+            var product = await _productService.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            await _productService.Remove(id);
+
+            return Ok(product);
+        }
     }
 }
